dcb11275d7c1e6c5ca00a7d5c4caccf3769f478a
leandro3742 <leandro.marrero@outlook.com>
2021-05-06 12:38:01 +0000

    primer commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,73 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState, useEffect } from "react";
+import { Link } from "react-dom";
 export function Home() {
+	const [name, setName] = useState("");
+	const [arr, setArr] = useState([]);
+	const [cambiar, setCambiar] = useState(false);
+
+	const agregarTarea = e => {
+		e.preventDefault();
+		let aux = arr;
+		aux.push(name);
+		setArr(aux);
+		setName("");
+	};
+
+	const eliminarName = index => {
+		let aux = arr;
+		while (index < aux.length - 1) {
+			aux[index] = aux[index + 1];
+			index++;
+		}
+		aux.length = index;
+		setArr(aux);
+		setCambiar(true);
+	};
+
+	useEffect(() => {
+		if (cambiar === true) {
+			setArr(arr);
+			setCambiar(false);
+		}
+	});
+
+	const style = {
+		borderBottom: "1px solid green"
+	};
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-secondary">Todo List</h1>
+			<form
+				onSubmit={agregarTarea}
+				className="d-flex justify-content-center">
+				<input
+					className="form-control col-3 "
+					placeholder="Agregar tarea"
+					onChange={e => setName(e.target.value)}
+					value={name}
+				/>
+			</form>
+			<div className="d-flex justify-content-center">
+				<div className="bg-dark text-warning col-3 rounded">
+					{arr.map((elem, index) => {
+						return (
+							<div
+								key={index}
+								className="d-flex justify-content-between"
+								style={style}>
+								<h2>{elem}</h2>
+								<div>
+									<button
+										className="btn btn-primary m-2"
+										onClick={() => eliminarName(index)}>
+										X
+									</button>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
