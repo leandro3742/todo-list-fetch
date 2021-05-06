90bf3c70717cb21654be3a37ee5955bcc418df91
leandro3742 <leandro.marrero@outlook.com>
2021-05-06 13:56:33 +0000

    terminado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-dom";
 export function Home() {
+	const style = {
+		borderBottom: "1px solid green"
+	};
+	const styleItems = {
+		borderBottom: "1px solid green"
+		// borderTop: "1px solid green"
+	};
 	const [name, setName] = useState("");
 	const [arr, setArr] = useState([]);
 	const [cambiar, setCambiar] = useState(false);
@@ -31,9 +38,6 @@ export function Home() {
 		}
 	});
 
-	const style = {
-		borderBottom: "1px solid green"
-	};
 	return (
 		<div className="text-center mt-5">
 			<h1 className="text-secondary">Todo List</h1>
@@ -41,33 +45,41 @@ export function Home() {
 				onSubmit={agregarTarea}
 				className="d-flex justify-content-center">
 				<input
-					className="form-control col-3 "
+					className="form-control col-5 "
 					placeholder="Agregar tarea"
 					onChange={e => setName(e.target.value)}
 					value={name}
 				/>
 			</form>
-			<div className="d-flex justify-content-center">
-				<div className="bg-dark text-warning col-3 rounded">
-					{arr.map((elem, index) => {
-						return (
-							<div
-								key={index}
-								className="d-flex justify-content-between"
-								style={style}>
-								<h2>{elem}</h2>
-								<div>
-									<button
-										className="btn btn-primary m-2"
-										onClick={() => eliminarName(index)}>
-										X
-									</button>
+			<div className="d-flex flex-column align-items-center">
+				<div className="rounded bg-dark col-5">
+					<div className="text-warning  ">
+						{arr.map((elem, index) => {
+							return (
+								<div
+									key={index}
+									className="d-flex justify-content-between mostrarBtn"
+									style={style}>
+									<h4 className="p-1">{elem}</h4>
+									<div>
+										<button
+											className={"btn a btn-primary m-2 "}
+											onClick={() => eliminarName(index)}>
+											X
+										</button>
+									</div>
 								</div>
-							</div>
-						);
-					})}
+							);
+						})}
+					</div>
+					<div
+						style={styleItems}
+						className="bg-dark rounded d-flex justify-content-start text-secondary">
+						<h6>{arr.length} Items</h6>
+					</div>
 				</div>
 			</div>
 		</div>
+		// </div>
 	);
 }
